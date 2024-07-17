13784ea9a0ad6880bd1731758065e0e00239c998
green <nzebene@universalperk.com>
2024-07-17 11:26:11 -0400

    fix/ changed company name on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
           <div className="footer-content">
             <ul className="footer-content__1">
               <li>
-                <span>CAR</span> Rental
+                <span>DAS</span> Topia
               </li>
               <li>
                 We offers a big range of vehicles for all your driving needs. We
