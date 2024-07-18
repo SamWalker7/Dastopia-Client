871b238d4a2ac34f7efe52428b10231a9d4fbb2c
tinsaye-up <tsimeneh@universalperk.com>
2024-07-18 21:27:41 +0300

    fix: teams page

--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -4,27 +4,53 @@ import PersonIcon from "@mui/icons-material/Person";
 
 function Team() {
   const teamPpl = [
-    { img: PersonIcon, name: "Abraham Wendmeneh", job: "Developer" },
-    { img: PersonIcon, name: "Michael Demeke", job: "Business Lead" },
-    { img: PersonIcon, name: "Sosina Yitay", job: "Marketer" },
-    { img: PersonIcon, name: "Dawit Aschalew", job: "Developer" },
-    { img: PersonIcon, name: "Biniam Haile", job: "Developer" },
-    { img: PersonIcon, name: "Yonathan Tesfaye", job: "Designer" },
-    { img: PersonIcon, name: "Samuel Derib", job: "Team Lead" },
-    { img: PersonIcon, name: "Dagimawi Woldesenbet", job: "Team Lead" },
-    { img: PersonIcon, name: "Naol Zebene", job: "Junior Software Developer" },
+    { img: PersonIcon, name: "Abraham Wendmeneh", job: "Role Placeholder" },
+    { img: PersonIcon, name: "Michael Demeke", job: "Role Placeholder" },
+    { img: PersonIcon, name: "Sosina Yitay", job: "Role Placeholder" },
+    { img: PersonIcon, name: "Dawit Aschalew", job: "Role Placeholder" },
+    { img: PersonIcon, name: "Biniam Haile", job: "Role Placeholder" },
+    { img: PersonIcon, name: "Yonathan Tesfaye", job: "Role Placeholder" },
+    { img: PersonIcon, name: "Naol Zebene", job: "Junior Software Engineer" },
     {
       img: PersonIcon,
       name: "Tinsaye Simeneh",
       job: "Junior Software Developer",
     },
   ];
+  const boardMembers = [
+    { img: PersonIcon, name: "Samuel Derib", job: "Board Member" },
+    { img: PersonIcon, name: "Dagimawi Woldesenbet", job: "Board Member" },
+    { img: PersonIcon, name: "Alemayehu Gemeda", job: "Board Member" },
+  ];
   return (
     <>
-      <section className="team-page" style={{paddingTop: "100px"}}>
+      <section className="team-page" style={{ paddingTop: "100px" }}>
         <div className="container">
           <div className="team-container">
             {teamPpl.map((ppl, id) => (
+              <div key={id} className="team-container__box">
+                <div className="team-container__box__img-div">
+                  <PersonIcon sx={{ fontSize: 100 }} />
+                </div>
+                <div className="team-container__box__descr">
+                  <h3>{ppl.name}</h3>
+                  <p>{ppl.job}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <p
+            style={{
+              fontSize: "2rem",
+              fontWeight: "bold",
+              marginBottom: "1rem",
+              textAlign: "center",
+            }}
+          >
+            Board Members
+          </p>
+          <div className="team-container">
+            {boardMembers.map((ppl, id) => (
               <div key={id} className="team-container__box">
                 <div className="team-container__box__img-div">
                   <PersonIcon sx={{ fontSize: 100 }} />
